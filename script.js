c498d12519cf503cf4c3a2349f0aee8da1dccb06
Dugald Steer <dugsteer@gmail.com>
2020-09-29 16:35:32 +0200

    Converting to Bingo Halloween

--- script.js
+++ script.js
@@ -3,10 +3,31 @@ const sidey = document.getElementById("conto");
 const blue = window.matchMedia("(max-width: 800px)");
 const ghostly = document.getElementById("ghostly");
 
+
 function ghostify(){
-  ghostly.innerHTML === '<img src="img/ghosties.png" alt="decorative">' ? 
-  ghostly.innerHTML = '<img src="img/ghosties2.png" alt="decorative">' : ghostly.innerHTML = '<img src="img/ghosties.png" alt="decorative">';
+  ghostly.innerHTML === '<img src="./img/funpic1.png" alt="decorative">' ? 
+  ghostly.innerHTML = '<img src="./img/funpic2.png" alt="decorative">' : ghostly.innerHTML = '<img src="./img/funpic1.png" alt="decorative">';
 }
+
+function getNewCards(){
+location.reload();
+}
+const loader = document.getElementById("loaderDiv");
+
+function stopLoader() {
+  loader.classList.add("stop");
+}
+
+
+
+function startLoader(){
+  loader.classList.remove("stop");
+  setTimeout(stopLoader, 1000);
+}
+
+setTimeout(stopLoader, 1000);
+
+
 
 function myFunction(x) {
   if (blue.matches) {
@@ -20,280 +41,321 @@ function toggler() {
 
 myFunction(blue);
 
-const loader = document.getElementById("loaderDiv");
+//GENERAL SCRIPT
 
-function stopLoader() {
-  loader.classList.add("stop");
-}
 
-function startLoader(){
-  loader.classList.remove("stop");
-  setTimeout(stopLoader, 1000);
-}
 
-setTimeout(stopLoader, 1000);
-// There are three different sets of words/images used in the game
-const firstSet = document.getElementById("set1");
-const secondSet = document.getElementById("set2");
-const thirdSet = document.getElementById("set3");
-
-// These are the other DOM elements
-// const instructions = document.getElementById("instructions");
-const gameContainer = document.getElementById("game-tiles");
-// const sideBar = document.getElementById("sidebar");
-// const sidebarText = document.getElementById("sidebarText");
-const page__title = document.getElementById("pageTitle");
-
-// Each image and word in a set array has the same data-framework number, in order to check for pairs.
-// The data-name is a string, which can be used in the second case as innerHTML.
-const set1 = [
-  { number: "1", name: "bat" },
-  { number: "1", name: "<img src = 'img/bat.svg' alt='bat' class='image'>" },
-  { number: "2", name: "bone" },
-  {
-    number: "2",
-    name: "<img src = 'img/bone.svg' alt='bone' class='image'>",
-  },
-  { number: "3", name: "broom" },
-  {
-    number: "3",
-    name: "<img src = 'img/broom.svg' alt='broom' class='image'>",
-  },
-  { number: "4", name: "werewolf" },
-  {
-    number: "4",
-    name: "<img src = 'img/werewolf.svg' alt='werewolf' class='image'>",
-  },
-  { number: "5", name: "candy" },
-  {
-    number: "5",
-    name: "<img src = 'img/candy.svg' alt='candy' class='image'>",
-  },
-  { number: "6", name: "cat" },
-  {
-    number: "6",
-    name: "<img src = 'img/cat.svg' alt='cat' class='image'>",
-  },
-  { number: "7", name: "cauldron" },
-  {
-    number: "7",
-    name: "<img src = 'img/cauldron.svg' alt='cauldron' class='image'>",
-  },
-  { number: "8", name: "clown" },
-  {
-    number: "8",
-    name: "<img src = 'img/clown.svg' alt='clown' class='image'>",
-  },
-];
-const set2 = [
-  { number: "9", name: "coffin" },
-  {
-    number: "9",
-    name: "<img src = 'img/coffin.svg' alt='coffin' class='image'>",
-  },
-  { number: "10", name: "witch" },
-  {
-    number: "10",
-    name: "<img src = 'img/witch.svg' alt='witch' class='image'>",
-  },
-  { number: "11", name: "fangs" },
-  {
-    number: "11",
-    name: "<img src = 'img/fangs.svg' alt='fangs' class='image'>",
-  },
-  { number: "12", name: "frank" },
-  {
-    number: "12",
-    name: "<img src = 'img/frank.svg' alt='frank' class='image'>",
-  },
-  { number: "13", name: "ghost" },
-  {
-    number: "13",
-    name: "<img src = 'img/ghost.svg' alt='ghost' class='image'>",
-  },
-  { number: "14", name: "grave" },
-  {
-    number: "14",
-    name: "<img src = 'img/grave.svg' alt='grave' class='image'>",
-  },
-  { number: "15", name: "hat" },
-  {
-    number: "15",
-    name: "<img src = 'img/hat.svg' alt='hat' class='image'>",
-  },
-  { number: "16", name: "monster" },
-  {
-    number: "16",
-    name: "<img src = 'img/monster.svg' alt='monster' class='image'>",
-  },
-];
-const set3 = [
-  { number: "17", name: "zombie" },
-  {
-    number: "17",
-    name: "<img src = 'img/zombie.svg' alt='zombie' class='image'>",
-  },
-  { number: "18", name: "mummy" },
-  {
-    number: "18",
-    name: "<img src = 'img/mummy.svg' alt='mummy' class='image'>",
-  },
-  { number: "19", name: "pirate" },
-  {
-    number: "19",
-    name: "<img src = 'img/pirate.svg' alt='pirate' class='image'>",
-  },
-  { number: "20", name: "pumpkin" },
-  {
-    number: "20",
-    name: "<img src = 'img/pumpkin.svg' alt='pumpkin' class='image'>",
-  },
-  { number: "21", name: "skeleton" },
-  {
-    number: "21",
-    name: "<img src = 'img/skeleton.svg' alt='skeleton' class='image'>",
-  },
-  { number: "22", name: "skull" },
-  {
-    number: "22",
-    name: "<img src = 'img/skull.svg' alt='skull' class='image'>",
-  },
-  { number: "23", name: "spider" },
-  {
-    number: "23",
-    name: "<img src = 'img/spider.svg' alt='spider' class='image'>",
-  },
-  { number: "24", name: "vampire" },
-  {
-    number: "24",
-    name: "<img src = 'img/vampire.svg' alt='vampire' class='image'>",
-  },
+var bingoArray = [
+  "bat.svg",
+  "bone.svg",
+  "broomstick.svg",
+  "candles.svg",
+  "candy.svg",
+  "cat.svg",
+  "cauldron.svg",
+  "clown.svg",
+  "coffin.svg",
+  "death.svg",
+  "fancy-dress.svg",
+  "fangs.svg",
+  "frankenstein.svg",
+  "ghost.svg",
+  "grave.svg",
+  "halloween.svg",
+  "hat.svg",
+  "monster.svg",
+  "moon.svg",
+  "mummy.svg",
+  "pirate.svg",
+  "pumpkin.svg",
+  "skeleton.svg",
+  "skull.svg",
+  "spell.svg",
+  "spider.svg",
+  "vampire.svg",
+  "wand.svg",
+  "web.svg",
+  "werewolf.svg",
+  "witch.svg",
+  "wizard.svg",
+  "zombie.svg",
 ];
 
-// By Default the game plays set1
-playGame(set1);
-
-// In the sidebar, different sets can be chosen by clicking
-firstSet.addEventListener("click", () => {
-  ghostify();
-  startLoader();
-  gameContainer.innerHTML = "";
-  page__title.textContent = "HALLOWEEN MATCHING: SET 1";
-  playGame(set1);
-});
-
-secondSet.addEventListener("click", () => {
-  ghostify();
-  startLoader();
-  gameContainer.innerHTML = "";
-  page__title.textContent = "HALLOWEEN MATCHING: SET 2";
-  playGame(set2);
-});
-
-thirdSet.addEventListener("click", () => {
-  ghostify();
-  startLoader();
-  gameContainer.innerHTML = "";
-  page__title.textContent = "HALLOWEEN MATCHING: SET 3";
-
-  playGame(set3);
-});
-
-// PLAY THE GAME
-// SHUFFLE THE GAME ARRAY for the chosen SET 1, 2 or 3.
-function playGame(array) {
-  function shuffle(array) {
-    array.sort(() => Math.random() - 0.5);
+function shuffle(bingoArray) {
+  var j, x, i;
+  for (i = bingoArray.length - 1; i > 0; i--) {
+    j = Math.floor(Math.random() * (i + 1));
+    x = bingoArray[i];
+    bingoArray[i] = bingoArray[j];
+    bingoArray[j] = x;
   }
-
-  // call the shuffle function & assign the array to "contents"
-  shuffle(array);
-  let contents = array;
-
-  // Create 16 Playing cards by using forEach on the array contents.
-  contents.forEach((data, index) => createCard(data, index));
-
-  // Call the createCard function turn the cards into DIV elements.
-  function createCard(data, index) {
-    let card = document.createElement("div");
-    // Add a classname so the cards can be styled with CSS
-    card.classList.add("card");
-    //  Include the data-famework (data.number) of the cards so they can be checked plus the necessary HTML.
-
-    card.innerHTML = `<div class="memory-card" tabindex=0 data-framework="${data.number}"><div class="front-face">${data.name}</div><div class="back-face"><img src = "img/tombstone.png" alt="picture of a basket of food" class="imgBg smaller" id="imgBg"></div></div>`;
-
-    // Append the card divs to the game container so they appear on the page.
-    gameContainer.appendChild(card);
-  }
-
-  // Select all instances of the class 'memory-card' and assign it to a constant
-  const cards = document.querySelectorAll(".memory-card");
-
-  // Set the initial boolean conditions to make the game work
-  let hasFlippedCard = false;
-  let lockBoard = false;
-  let firstCard, secondCard;
-
-  // Prevent card flipping, add the 'flip' CSS class to flip the card if it's alright to do so.
-  function flipCard() {
-    if (lockBoard) return;
-    if (this === firstCard) return;
-
-    this.classList.add("flip");
-
-    // Allowing two cards to be flipped and calling the check function
-    if (!hasFlippedCard) {
-      hasFlippedCard = true;
-      firstCard = this;
-      return;
-    }
-
-    secondCard = this;
-
-    checkForMatch();
-  }
-
-  // The check function
-  function checkForMatch() {
-    if (firstCard.dataset.framework === secondCard.dataset.framework) {
-      disableCards();
-      return;
-    }
-
-    unflipCards();
-  }
-
-  // Make pairs vanish by killing their innerHTML
-  function disableCards() {
-    lockBoard = true;
-    setTimeout(() => {
-      firstCard.innerHTML = "";
-      secondCard.innerHTML = "";
-      resetBoard();
-    }, 1000);
-  }
-
-  // Unflip cards if they don't match by removing the flip class.
-  function unflipCards() {
-    lockBoard = true;
-    setTimeout(() => {
-      firstCard.classList.remove("flip");
-      secondCard.classList.remove("flip");
-      resetBoard();
-    }, 1000);
-  }
-
-  // Set the board to continue the game, setting the constants to their original state
-  function resetBoard() {
-    [hasFlippedCard, lockBoard] = [false, false];
-    [firstCard, secondCard] = [null, null];
-  }
-
-  // Event listener added to the cards
-  cards.forEach((card) => card.addEventListener("click", flipCard));
-
-  // RESTART COMPLETELY
-  // document.getElementById("startButton").addEventListener("click", function () {
-  //   location.reload();
-  // });
+  return bingoArray;
 }
+
+function shuffle(redArray) {
+  var j, x, i;
+  for (i = redArray.length - 1; i > 0; i--) {
+    j = Math.floor(Math.random() * (i + 1));
+    x = redArray[i];
+    redArray[i] = redArray[j];
+    redArray[j] = x;
+  }
+  return redArray;
+}
+
+function shuffle(greenArray) {
+  var j, x, i;
+  for (i = greenArray.length - 1; i > 0; i--) {
+    j = Math.floor(Math.random() * (i + 1));
+    x = greenArray[i];
+    greenArray[i] = greenArray[j];
+    greenArray[j] = x;
+  }
+  return greenArray;
+}
+function shuffle(silverArray) {
+  var j, x, i;
+  for (i = silverArray.length - 1; i > 0; i--) {
+    j = Math.floor(Math.random() * (i + 1));
+    x = silverArray[i];
+    silverArray[i] = silverArray[j];
+    silverArray[j] = x;
+  }
+  return silverArray;
+}
+function shuffle(orangeArray) {
+  var j, x, i;
+  for (i = orangeArray.length - 1; i > 0; i--) {
+    j = Math.floor(Math.random() * (i + 1));
+    x = orangeArray[i];
+    orangeArray[i] = orangeArray[j];
+    orangeArray[j] = x;
+  }
+  return orangeArray;
+}
+function shuffle(blackArray) {
+  var j, x, i;
+  for (i = blackArray.length - 1; i > 0; i--) {
+    j = Math.floor(Math.random() * (i + 1));
+    x = blackArray[i];
+    blackArray[i] = blackArray[j];
+    blackArray[j] = x;
+  }
+  return blackArray;
+}
+function shuffle(puceArray) {
+  var j, x, i;
+  for (i = puceArray.length - 1; i > 0; i--) {
+    j = Math.floor(Math.random() * (i + 1));
+    x = puceArray[i];
+    puceArray[i] = puceArray[j];
+    puceArray[j] = x;
+  }
+  return puceArray;
+}
+function shuffle(carrotArray) {
+  var j, x, i;
+  for (i = carrotArray.length - 1; i > 0; i--) {
+    j = Math.floor(Math.random() * (i + 1));
+    x = carrotArray[i];
+    carrotArray[i] = carrotArray[j];
+    carrotArray[j] = x;
+  }
+  return carrotArray;
+}
+
+shuffle(bingoArray);
+var newArray = bingoArray.slice(0, 12);
+var blueArray = newArray;
+pastePics();
+
+var redArray = bingoArray;
+shuffle(redArray);
+var blueArray = redArray.slice(0, 12);
+pastePics1();
+
+var greenArray = bingoArray;
+shuffle(greenArray);
+var pinkArray = greenArray.slice(0, 12);
+pastePics2();
+
+var blackArray = bingoArray;
+shuffle(blackArray);
+var whiteArray = blackArray.slice(0, 12);
+pastePics3();
+
+var puceArray = bingoArray;
+shuffle(puceArray);
+var greyArray = puceArray.slice(0, 12);
+pastePics4();
+
+var carrotArray = bingoArray;
+shuffle(carrotArray);
+var brownArray = carrotArray.slice(0, 12);
+pastePics5();
+
+function pastePics() {
+  document.getElementById("id1").innerHTML = `<img src="img/${newArray[0]}">`;
+  document.getElementById("id2").innerHTML = `<img src="img/${newArray[1]}">`;
+  document.getElementById("id3").innerHTML = `<img src="img/${newArray[2]}">`;
+  document.getElementById("id4").innerHTML = `<img src="img/${newArray[3]}">`;
+  document.getElementById("id5").innerHTML = `<img src="img/${newArray[4]}">`;
+  document.getElementById("id6").innerHTML = `<img src="img/${newArray[5]}">`;
+  document.getElementById("id7").innerHTML = `<img src="img/${newArray[6]}">`;
+  document.getElementById("id8").innerHTML = `<img src="img/${newArray[7]}">`;
+  document.getElementById("id9").innerHTML = `<img src="img/${newArray[8]}">`;
+  document.getElementById("id10").innerHTML = `<img src="img/${newArray[9]}">`;
+  document.getElementById("id11").innerHTML = `<img src="img/${newArray[10]}">`;
+  document.getElementById("id12").innerHTML = `<img src="img/${newArray[11]}">`;
+}
+
+function pastePics1() {
+  document.getElementById("id13").innerHTML = `<img src="img/${blueArray[0]}">`;
+  document.getElementById("id14").innerHTML = `<img src="img/${blueArray[1]}">`;
+  document.getElementById("id15").innerHTML = `<img src="img/${blueArray[2]}">`;
+  document.getElementById("id16").innerHTML = `<img src="img/${blueArray[3]}">`;
+  document.getElementById("id17").innerHTML = `<img src="img/${blueArray[4]}">`;
+  document.getElementById("id18").innerHTML = `<img src="img/${blueArray[5]}">`;
+  document.getElementById("id19").innerHTML = `<img src="img/${blueArray[6]}">`;
+  document.getElementById("id20").innerHTML = `<img src="img/${blueArray[7]}">`;
+  document.getElementById("id21").innerHTML = `<img src="img/${blueArray[8]}">`;
+  document.getElementById("id22").innerHTML = `<img src="img/${blueArray[9]}">`;
+  document.getElementById(
+    "id23"
+  ).innerHTML = `<img src="img/${blueArray[10]}">`;
+  document.getElementById(
+    "id24"
+  ).innerHTML = `<img src="img/${blueArray[11]}">`;
+}
+function pastePics2() {
+  document.getElementById("id25").innerHTML = `<img src="img/${pinkArray[0]}">`;
+  document.getElementById("id26").innerHTML = `<img src="img/${pinkArray[1]}">`;
+  document.getElementById("id27").innerHTML = `<img src="img/${pinkArray[2]}">`;
+  document.getElementById("id28").innerHTML = `<img src="img/${pinkArray[3]}">`;
+  document.getElementById("id29").innerHTML = `<img src="img/${pinkArray[4]}">`;
+  document.getElementById("id30").innerHTML = `<img src="img/${pinkArray[5]}">`;
+  document.getElementById("id31").innerHTML = `<img src="img/${pinkArray[6]}">`;
+  document.getElementById("id32").innerHTML = `<img src="img/${pinkArray[7]}">`;
+  document.getElementById("id33").innerHTML = `<img src="img/${pinkArray[8]}">`;
+  document.getElementById("id34").innerHTML = `<img src="img/${pinkArray[9]}">`;
+  document.getElementById(
+    "id35"
+  ).innerHTML = `<img src="img/${pinkArray[10]}">`;
+  document.getElementById(
+    "id36"
+  ).innerHTML = `<img src="img/${pinkArray[11]}">`;
+}
+function pastePics3() {
+  document.getElementById(
+    "id37"
+  ).innerHTML = `<img src="img/${whiteArray[0]}">`;
+  document.getElementById(
+    "id38"
+  ).innerHTML = `<img src="img/${whiteArray[1]}">`;
+  document.getElementById(
+    "id39"
+  ).innerHTML = `<img src="img/${whiteArray[2]}">`;
+  document.getElementById(
+    "id40"
+  ).innerHTML = `<img src="img/${whiteArray[3]}">`;
+  document.getElementById(
+    "id41"
+  ).innerHTML = `<img src="img/${whiteArray[4]}">`;
+  document.getElementById(
+    "id42"
+  ).innerHTML = `<img src="img/${whiteArray[5]}">`;
+  document.getElementById(
+    "id43"
+  ).innerHTML = `<img src="img/${whiteArray[6]}">`;
+  document.getElementById(
+    "id44"
+  ).innerHTML = `<img src="img/${whiteArray[7]}">`;
+  document.getElementById(
+    "id45"
+  ).innerHTML = `<img src="img/${whiteArray[8]}">`;
+  document.getElementById(
+    "id46"
+  ).innerHTML = `<img src="img/${whiteArray[9]}">`;
+  document.getElementById(
+    "id47"
+  ).innerHTML = `<img src="img/${whiteArray[10]}">`;
+  document.getElementById(
+    "id48"
+  ).innerHTML = `<img src="img/${whiteArray[11]}">`;
+}
+function pastePics4() {
+  document.getElementById("id49").innerHTML = `<img src="img/${greyArray[0]}">`;
+  document.getElementById("id50").innerHTML = `<img src="img/${greyArray[1]}">`;
+  document.getElementById("id51").innerHTML = `<img src="img/${greyArray[2]}">`;
+  document.getElementById("id52").innerHTML = `<img src="img/${greyArray[3]}">`;
+  document.getElementById("id53").innerHTML = `<img src="img/${greyArray[4]}">`;
+  document.getElementById("id54").innerHTML = `<img src="img/${greyArray[5]}">`;
+  document.getElementById("id55").innerHTML = `<img src="img/${greyArray[6]}">`;
+  document.getElementById("id56").innerHTML = `<img src="img/${greyArray[7]}">`;
+  document.getElementById("id57").innerHTML = `<img src="img/${greyArray[8]}">`;
+  document.getElementById("id58").innerHTML = `<img src="img/${greyArray[9]}">`;
+  document.getElementById(
+    "id59"
+  ).innerHTML = `<img src="img/${greyArray[10]}">`;
+  document.getElementById(
+    "id60"
+  ).innerHTML = `<img src="img/${greyArray[11]}">`;
+}
+function pastePics5() {
+  document.getElementById(
+    "id61"
+  ).innerHTML = `<img src="img/${brownArray[0]}">`;
+  document.getElementById(
+    "id62"
+  ).innerHTML = `<img src="img/${brownArray[1]}">`;
+  document.getElementById(
+    "id63"
+  ).innerHTML = `<img src="img/${brownArray[2]}">`;
+  document.getElementById(
+    "id64"
+  ).innerHTML = `<img src="img/${brownArray[3]}">`;
+  document.getElementById(
+    "id65"
+  ).innerHTML = `<img src="img/${brownArray[4]}">`;
+  document.getElementById(
+    "id66"
+  ).innerHTML = `<img src="img/${brownArray[5]}">`;
+  document.getElementById(
+    "id67"
+  ).innerHTML = `<img src="img/${brownArray[6]}">`;
+  document.getElementById(
+    "id68"
+  ).innerHTML = `<img src="img/${brownArray[7]}">`;
+  document.getElementById(
+    "id69"
+  ).innerHTML = `<img src="img/${brownArray[8]}">`;
+  document.getElementById(
+    "id70"
+  ).innerHTML = `<img src="img/${brownArray[9]}">`;
+  document.getElementById(
+    "id71"
+  ).innerHTML = `<img src="img/${brownArray[10]}">`;
+  document.getElementById(
+    "id72"
+  ).innerHTML = `<img src="img/${brownArray[11]}">`;
+}
+
+function printdiv() {
+  var headstr = "BINGO CARDS";
+  var footstr = "More bingo at www.esl-ology.com";
+  var newstr = document.getElementById("container").innerHTML;
+  var oldstr = document.body.innerHTML;
+  document.body.innerHTML = headstr + newstr + footstr;
+  window.print();
+  document.body.innerHTML = oldstr;
+  return false;
+}
+
+
+//TOGGLE SIDEBAR
+
+// const sideBar = document.getElementById("infobox");
+
+// function openSidebar() {
+//   sideBar.classList.toggle("moveinfobox");
+// }
